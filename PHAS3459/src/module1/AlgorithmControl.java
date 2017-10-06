bc1af7cb4d9cb78d6913418469af481df373e800
zcaprma <zcaprma@CT00006686.ad.ucl.ac.uk>
2017-10-06 13:00:29 +0100

    Finish Timer

--- PHAS3459/src/module1/AlgorithmControl.java
+++ PHAS3459/src/module1/AlgorithmControl.java
@@ -2,6 +2,7 @@ package module1;
 
 public class AlgorithmControl {
 	
+	//print integers between a and b in steps of 1
 	public void loop(int a, int b) {
 		int i;
 		int imax = b;
@@ -10,11 +11,12 @@ public class AlgorithmControl {
 		}
 	}
 	
+	//print integers between a and b in steps of -1
 	public void decrement(int a, int b) {
-		 boolean y;
+		 boolean y; //while loop requires a boolean
 		 float x = a;
 		 y = true;
-		 
+		 //runs while y statement is true
 		 while (y) {
 			 x--;
 			 y = x>b ;
@@ -22,6 +24,7 @@ public class AlgorithmControl {
 		 }
 	}
 	
+	//print integers between a and b in steps of c
 	public void increment(double a, double b, double c) {
 		double i;
 		for (i = a ; i <= b ;  i = i + c)  {
@@ -30,30 +33,41 @@ public class AlgorithmControl {
 		
 	}
 	
-	public void timer() {
-		long timeNow = System.currentTimeMillis();
-		//int a =1;
+	//timer to record for c milliseconds in d intervals
+	public int timer(int c, int d) {
+		long timeNow = System.currentTimeMillis(); //time at start of loop
+		int a = 0;
 		boolean x;
-		long time = 0;
+		long time = 0;//initialise time
 		x = true;
 		while(x) {
-			time = System.currentTimeMillis();
-			x=time % timeNow != 10000;
-				//a++;
+			time = System.currentTimeMillis();//updated time
+			x=time % timeNow < c; //runs until the remainder is the total time 
+			//prints every 'd' steps
+			if (a % d == 0) { //when remainder is zero - print & increase a
+				System.out.println(a);
+				a=a+1;
+			}
+			else { //when remainder isnt zero - increase a
+				a=a+1;
+			}
 		}
-		System.out.println(time);
-			
-			
-
+		return (a);//statement returns the total number of steps
 	}
 
 	public static void main(String[] args) {
 		AlgorithmControl ac = new AlgorithmControl();
 		
-		//ac.loop(1, 10);
-		//ac.increment(2.4, 8.8 , 0.20);
-		//ac.decrement(2,  -12);
-		ac.timer();
+		ac.loop(1, 10);//integers 1 to 10
+		ac.increment(2.4, 8.8 , 0.20); //numbers 2.4 to 8.8 in steps of 0.2
+		ac.decrement(2,  -12);//integers 2 to -12
+		
+		//total loops for 8 seconds in steps of 40000
+		int totalloop1 = ac.timer(8000, 40000);
+		//total loops for 8 seconds in steps of 1000
+		int totalloop2 = ac.timer(8000, 1000);
+		
+		System.out.println("the total number of loops for steps of 40 000 and 1000 9s "+ totalloop1 + " and " + totalloop2 +"respectively" );
 		
 
 	}
