316c7b32ed7a416e4e09bcda91932cda219ac28b
Rosie Mason <zcaprma@ucl.ac.uk>
2017-10-13 12:18:36 +0100

    Test the complex function

---
+++ PHAS3459/src/module2/TestComplex.java
@@ -0,0 +1,20 @@
+package module2;
+
+public class TestComplex {
+
+	public static void main(String[] args) {
+		Complex c1 = new Complex(1, 2);
+		Complex c2 = new Complex(-2, -1);
+		
+		System.out.println("The product of c1 and c2 is"+Complex.multiply(c1,  c2));
+		System.out.println("The ratio of c1 / c2 is "+Complex.divide(c1,  c2));
+		System.out.println("The product of c1 and I is" + Complex.multiply(c1, Complex.I));
+		System.out.println("The ratio of c1/0 is " + Complex.divide(c1, Complex.ZERO));
+		System.out.println("The product of c1 with the conugate of c1 is "+ Complex.multiply(c1,  c1.conjugate()));
+		System.out.println("The product of c2 with the conjugate of c2 is "+Complex.multiply(c2, c2.conjugate()));
+		
+		
+
+	}
+
+}
