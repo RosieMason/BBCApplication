1fc9d6b86ba3d5b0090b16719adc451f1a1e0e1b
Rosie Mason <zcaprma@ucl.ac.uk>
2017-10-13 12:59:00 +0100

    Set up initial parameters for a falling particle

---
+++ PHAS3459/src/module2/ParticleMain.java
@@ -0,0 +1,16 @@
+package module2;
+
+public class ParticleMain {
+
+	public static void main(String[] args) {
+		
+		FallingParticle f = new FallingParticle(5.2, 3.6);
+		
+		f.setH(10); f.setV(10);
+		//double a = FallingParticle.drop(0.5);
+		
+		
+
+	}
+
+}
