72c17416efcc73badc25aed92ec4681b0e792eb0
Rosie Mason <zcaprma@ucl.ac.uk>
2017-10-15 14:15:38 +0100

    Add an equation for z

--- PHAS3459/src/module2/ParticleMain.java
+++ PHAS3459/src/module2/ParticleMain.java
@@ -5,12 +5,23 @@ public class ParticleMain {
 	public static void main(String[] args) {
 		
 		FallingParticle f = new FallingParticle(5.2, 3.6);
-		
-		f.setH(10); f.setV(10);
-		//double a = FallingParticle.drop(0.5);
+		//f.setH(10); 
 		
 		
+		f.drop(0.5);
+		System.out.println(f.getT());
+		
+		f.drop(0.1);
+		System.out.println(f.getT());
 
+		f.drop(0.01);
+		System.out.println(f.getT());
+		
+		f.drop(0.001);
+		System.out.println(f.getT());
+		
+		f.drop(0.0001);
+		System.out.println(f.getT());
 	}
 
 }
