c17fbeda9788c419ad0ac6a6e9d4bd88835be3a9
Rosie Mason <zcaprma@ucl.ac.uk>
2017-10-15 14:16:24 +0100

    add inital value h = 1

--- PHAS3459/src/module2/ParticleMain.java
+++ PHAS3459/src/module2/ParticleMain.java
@@ -5,7 +5,7 @@ public class ParticleMain {
 	public static void main(String[] args) {
 		
 		FallingParticle f = new FallingParticle(5.2, 3.6);
-		//f.setH(10); 
+		f.setH(10); 
 		
 		
 		f.drop(0.5);
