119b0775862b05cb6dcc0ca39a0aa5ac7bab985c
Rosie Mason <zcaprma@ucl.ac.uk>
2017-10-15 14:34:05 +0100

    Fully Commented

--- PHAS3459/src/module2/TestThreeVector.java
+++ PHAS3459/src/module2/TestThreeVector.java
@@ -3,6 +3,7 @@ package module2;
 public class TestThreeVector {
 
 	public static void main(String[] args) {
+		// create new three vectors 
 		ThreeVector v1 = new ThreeVector(3, 5, 2);
 		ThreeVector v2 = new ThreeVector(2, 4, 1);
 		ThreeVector v3 = new ThreeVector(0, 0, 0);
