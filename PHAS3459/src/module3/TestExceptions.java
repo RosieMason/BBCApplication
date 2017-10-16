842371f00abfc582709c19347bfcf168b05064a7
Rosie Mason <zcaprma@ucl.ac.uk>
2017-10-16 16:48:35 +0100

    Module 3 - Test Exceptions
    Test the timestep cannot be larger than zero exception

--- PHAS3459/src/module3/TestExceptions.java
+++ PHAS3459/src/module3/TestExceptions.java
@@ -3,56 +3,56 @@ package module3;
 public class TestExceptions {
 
 	public static void main(String[] args) { 
-//		Complex c1 = Complex.ZERO;
-//		Complex c2 = new Complex(1, 2);
-//		try {
-//			 Complex c = c1.normalised();
-//			 System.out.println(c);
-//		}
-//		catch (Exception e) {
-//		System.out.println(e);
-//		}
-//		try {
-//			 Complex c = Complex.divide(c2, c1);
-//			 System.out.println(c);
-//		}
-//		catch (Exception e) {
-//			System.out.println(e);
-//		}
-//		
-//		ThreeVector v1 = new ThreeVector(0, 0, 0);
-//		ThreeVector v2 = new ThreeVector(2, 4, 1);
-//		try {
-//			ThreeVector t = v1.unitVector();
-//			System.out.println(t);
-//		}
-//		catch (Exception e) {
-//			System.out.println(e);
-//		}
-//		
-//		try {
-//			double a = v2.angle(v1);
-//			System.out.println(a);
-//		}
-//		catch (Exception e) {
-//			System.out.println(e);
-//		}
-//		
-//		try{
-//			FallingParticle f = new FallingParticle(-2.0, 3.6);
-//			System.out.println(f);
-//		}
-//		catch (Exception e) {
-//			System.out.println(e);
-//		}
-//		
-//		try{
-//			FallingParticle f = new FallingParticle(2.0, -3.6);
-//			System.out.println(f);
-//		}
-//		catch (Exception e) {
-//			System.out.println(e);
-//		}
+		Complex c1 = Complex.ZERO;
+		Complex c2 = new Complex(1, 2);
+		try {
+			 Complex c = c1.normalised();
+			 System.out.println(c);
+		}
+		catch (Exception e) {
+		System.out.println(e);
+		}
+		try {
+			 Complex c = Complex.divide(c2, c1);
+			 System.out.println(c);
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		ThreeVector v1 = new ThreeVector(0, 0, 0);
+		ThreeVector v2 = new ThreeVector(2, 4, 1);
+		try {
+			ThreeVector t = v1.unitVector();
+			System.out.println(t);
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		try {
+			double a = v2.angle(v1);
+			System.out.println(a);
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		try{
+			FallingParticle f = new FallingParticle(-2.0, 3.6);
+			System.out.println(f);
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		try{
+			FallingParticle f = new FallingParticle(2.0, -3.6);
+			System.out.println(f);
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
 		
 		try{
 			FallingParticle f = new FallingParticle(5.2, 3.6);
@@ -60,8 +60,28 @@ public class TestExceptions {
 		}
 		
 		catch (Exception e) {			
+		System.out.println(e);
+		}
+		
+		try {
+			FallingParticle f = new FallingParticle(5.2, 3.6);
+			f.doTimeStep(-2.0);
+		}
+		catch (Exception e) {			
 			System.out.println(e);
 		}
+		
+		try {
+			FallingParticle f = new FallingParticle(5.2, 3.6);
+			f.drop(-2.0);
+		}
+		catch (Exception e) {			
+			System.out.println(e);
+		}
+		
+		
+
+		
 
 		
 		
