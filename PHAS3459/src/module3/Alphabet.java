8455beb82ca1aafe5cc4293843516a95c36c1fa0
Rosie Mason <zcaprma@ucl.ac.uk>
2017-10-20 11:43:56 +0100

    Module 3 - Alphabet - create randomCharacter method - outputs a char

--- PHAS3459/src/module3/Alphabet.java
+++ PHAS3459/src/module3/Alphabet.java
@@ -1,10 +1,24 @@
 package module3;
 
+import java.util.Random;
+import java.lang.Character;
+import java.lang.Integer;
+import java.lang.StringBuilder;
+import java.lang.Math;
+
 public class Alphabet {
+	
+	public static char randomCharacter() {
+		Random n = new Random();
+		int i = n.nextInt();
+		char c =(char) i;
+		return c;
+	}
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Random a = new Random();
+		System.out.println(randomCharacter());
 	}
 
 }
