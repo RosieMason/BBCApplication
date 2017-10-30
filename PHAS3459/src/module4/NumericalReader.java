1cdb39c026794de9c308053a881aaae06e42b2ab
Rosie Mason <zcaprma@ucl.ac.uk>
2017-10-30 11:47:19 +0000

    Module 4 - NumericalReader - create methods
    Create Methods to input string, analysis start, analyseData and analysis
    end
    
    Problems - doesnt work with 'saveFile' in analyseData and analysis end

--- PHAS3459/src/module4/NumericalReader.java
+++ PHAS3459/src/module4/NumericalReader.java
@@ -9,27 +9,29 @@ public class NumericalReader {
 	
 	public NumericalReader() {
 	}
+	//initialise variables
+	Double minValue = null;
+	Double maxValue = null;
+	Double nValues = 0.;
+	Double sumOfValues = 0.;
+	File outputfile;
+	String dataFile;
+	String saveFile;
 	
-	double minValue;
-	double maxValue;
-	double nValues;
-	double sumOfValues;
-	
-	public static String getStringFromKeyboard() {
+	//method to take information from the keyboard
+	public static String getStringFromKeyboard() throws Exception {
+		//if no input throw exception
+		if (System.in == null) {
+			throw new Exception("Nothing was typed");
+		}
 		InputStreamReader r = new InputStreamReader(System.in);
 		BufferedReader b = new BufferedReader(r);
 		System.out.println("Please type something");
-		String typed = null;
-		try {
-			String s = b.readLine();
-			typed = s;
-
-		 }
-		 catch (IOException e) {
-		 }
-		return typed;
+		String s = b.readLine();
+		return s;
 	}
 	
+	//method to read data from a url 
 	public BufferedReader brFromURL(String urlName) throws IOException {
 		URL u = new URL(urlName);
 		InputStream is = u.openStream();
@@ -38,47 +40,129 @@ public class NumericalReader {
 		return b;
 	}
 	
+	//method to initialise files and variables 
 	void analysisStart(String dataFile) throws IOException{
-		double minValue;
-		double maxValue;
-		double nValues;
-		double sumOfValues;
 		File outputfile = new File(dataFile);
+		Double minValue;
+		Double maxValue;
+		Double nValues;
+		Double sumOfValues;
 		}
 	
+	//method to analys the data in the file
 	void analyseData(String line) throws IOException {
-		Scanner s = new Scanner(line);
-		File outputfile = new File(line);
-		while(s.hasNext()) {
-			line = s.nextLine();
-			if (line.startsWith("[^\\d.]")) {
-				s.nextLine();
-			}
-			else {
-				try(
-						FileWriter fw = new FileWriter(outputfile);
-						PrintWriter pw = new PrintWriter(fw);){
-					double a = s.nextDouble(); 
-					if(a < minValue) {
-						minValue = a;
-					}
-					else if (a > maxValue) {
-						maxValue = a;
-					}
-					
-					nValues++;
-					sumOfValues = sumOfValues + a;
-					
-					
+		Scanner scan = new Scanner(line);
+		//initial min and max
+		double b = scan.nextDouble();
+		minValue = b;
+		maxValue = b;
+		//if the line start with a number - analyse
+			if(Character.isDigit(line.charAt(0))) {
+				try (
+						FileWriter fw = new FileWriter(saveFile);
+						BufferedWriter br = new BufferedWriter(fw);
+						PrintWriter pw = new PrintWriter(br);){;
+					//while there are still elements in the line continue analysis
+					while (scan.hasNext()) {
+						double a = scan.nextDouble();
+						System.out.println (a);
+						//if a is smaller than the min, a is the new min
+						if (a < minValue) {
+							minValue = a;
+						}
+						//if a is greater than the max, a is the new max
+						else if(a>maxValue) {
+							maxValue = a;
+						}
+						//increase n values by 1
+						nValues++;
+						//add integer to the sumOfValues
+						sumOfValues = sumOfValues + a;
+						//write new values to file
+						pw.write(Double.toString(minValue));
+						pw.write(Double.toString(maxValue));
+						pw.write(Double.toString(nValues));
+						pw.write(Double.toString(sumOfValues));
 					}
 				}
 			}
-
-			
+			//if the line doesnt start with a number - do nothing
+			else {
+			}
+	
 		}
 
-	public static void main(String[] args) {
+
+	//method to print file at the end of analysis
+	void analysisEnd() throws IOException {
+		FileReader fr = new FileReader(saveFile);
+		BufferedReader br = new BufferedReader(fr);
+		Scanner s = new Scanner(br);
+		minValue = s.nextDouble();
+		System.out.println("The minimum value is : "+minValue);
+		s.next();
+		maxValue = s.nextDouble();
+		System.out.println("The maximum value is : "+maxValue);
+		s.next();
+		nValues = s.nextDouble();
+		System.out.println("The total number of values is : "+nValues);
+		s.next();
+		sumOfValues = s.nextDouble();
+		System.out.println("The sum of the values is : "+sumOfValues);
+		
+		
+
+		
+		 
 
 	}
+	
 
+	public static void main(String[] args) {
+		//initalise directory 
+		String directory;
+		//try to input directory
+		try {
+			directory = getStringFromKeyboard();
+		}
+		//catch exception if there is no input
+		catch(Exception e) {
+			directory = "System.getProperty(\"user.home\")";
+			System.out.println(e);
+		}
+		NumericalReader nr = new NumericalReader();
+		
+		
+		//try to create, analyse and print file for URL1
+		try {
+			BufferedReader br = nr.brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_data1.txt");
+			String saveFile = (directory + File.separator + "Eclipse" + File.separator+"numberFromURL1.txt");
+			String line = "";
+			nr.analysisStart(saveFile);
+			while ((line = br.readLine()) !=null){
+				nr.analyseData(line);
+			}
+			nr.analysisEnd();
+		}
+		//catch exception if thrown
+		catch(IOException e){
+		}
+		
+		
+		//try to create, analyse and print file for URL1
+		try {
+			BufferedReader br = nr.brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_data2.txt");
+			String saveFile = (directory + File.separator + "Eclipse" + File.separator+"numberFromURL2.txt");
+			String line = "";
+			nr.analysisStart(saveFile);
+			while ((line = br.readLine()) !=null){
+				nr.analyseData(line);
+			}
+			nr.analysisEnd();
+		}
+		//catch exception if thrown
+		catch(IOException e){
+		}
+
+}
 }
