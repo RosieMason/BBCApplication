aab4e1c2a550b347dd40b186aa47760fd8ec2fa0
rosiemason <rosiemason@192.168.0.10>
2017-11-11 15:38:18 +0000

    Module 5 - Theory
    method to set n
    calculate y

---
+++ PHAS3459/src/module5/Theory.java
@@ -0,0 +1,21 @@
+package module5;
+
+public class Theory {
+	
+	double n;
+	
+	void setN(double x) {
+		n = x;
+	}
+	public double y(double x) {
+		double y = Math.pow(x, n);
+		return y;
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
