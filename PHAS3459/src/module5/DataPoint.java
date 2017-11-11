bffa5612f08b30316554d7f4422f99b7a4cd8cae
rosiemason <rosiemason@192.168.0.10>
2017-11-11 10:31:14 +0000

    Module 5  - DataPoints - Create Class
    Code to set x, y and ey values from a url

---
+++ PHAS3459/src/module5/DataPoint.java
@@ -0,0 +1,69 @@
+package module5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.*;
+
+public class DataPoint {
+	
+	static ArrayList<Double> x = new ArrayList<Double>();
+	static ArrayList<Double> y = new ArrayList<Double>();
+	static ArrayList<Double> ey = new ArrayList<Double>();
+	
+	public static BufferedReader brFromURL (String urlName) throws IOException {
+		URL u = new URL(urlName);
+		InputStream is = u.openStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader b = new BufferedReader(isr);
+		return b;
+	}
+	
+	public static ArrayList setX(BufferedReader dataAsBR) throws IOException{
+		try (
+			Scanner s = new Scanner(dataAsBR);
+		){
+			while (s.hasNextLine()){
+				x.add(s.nextDouble());
+				s.nextLine();
+			}
+			return x;
+		}
+	}
+	
+	public static ArrayList setY(BufferedReader dataAsBR) throws IOException{
+		try (
+			Scanner s = new Scanner(dataAsBR);
+		){
+			while (s.hasNextLine()){
+				s.next();
+				y.add(s.nextDouble());
+				s.nextLine();
+			}
+			return y;
+		}
+	}
+	
+	public static ArrayList setEY(BufferedReader dataAsBR) throws IOException{
+		try (
+			Scanner s = new Scanner(dataAsBR);
+		){
+			while (s.hasNextLine()){
+				s.next();
+				s.next();
+				ey.add(s.nextDouble());
+				s.nextLine();
+			}
+			return ey;
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
